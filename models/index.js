7d192e93ba3fc1dc4cefa44b4acb7350637bcfab
Mustafa <janodones@gmail.com>
2024-02-24 22:52:31 +0300

    working version

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require('./User');
+const Comments = require('./Comments');
+
+User.hasMany(Comments, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Comments.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = {User,Comments};
